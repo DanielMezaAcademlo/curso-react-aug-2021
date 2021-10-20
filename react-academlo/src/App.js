9f566eec6c709bb588284858f648b3ead8bb770c
= <danielmezauaeh@gmail.com>
2021-10-19 21:33:16 -0500

    Primeros conceptos de React, instalacion y analisis del contenido de la carpeta

---
+++ react-academlo/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+//Components
+import Header from "./componentes/Header";
+
+function App() {
+  //JSX
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+
+//Props, state, hooks
---
+++ react-academlo/src/componentes/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  );
+};
+
+export default Menu;
