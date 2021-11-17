2909fc606c9ef2fecd5db6b04afb358ad94d21a9
danielmezam <danielmezauaeh@gmail.com>
2021-11-16 20:27:26 -0600

    useReducer basico

--- react-academlo/src/App.js
+++ react-academlo/src/App.js
@@ -1,18 +1,38 @@
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
-//Components
-import Header from "./componentes/Header";
+const initialState = { counter: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return {
+        counter: state.counter + 1
+      };
+    case "subs":
+      return {
+        counter: state.counter - 1
+      };
+    default:
+      break;
+  }
+};
 
 function App() {
-  //JSX
+  //UseState
+  // const [counter, setCounter] = useState(0);
+  //useReducer
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
-      <Header />
+      {/* <button onClick={() => setCounter(counter + 1)}>Add 1</button>
+      <button onClick={() => setCounter(counter - 1)}>subs 1</button> */}
+      <button onClick={() => dispatch({ type: "add" })}>Add 1</button>
+      <button onClick={() => dispatch({ type: "subs" })}>subs 1</button>
+      <p>{state.counter}</p>
     </div>
   );
 }
 
 export default App;
-
-//Props, state, hooks
